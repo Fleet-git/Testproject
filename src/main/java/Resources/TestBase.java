7d4d7d1a98d7b2f930c58bbe07895dae9ed59bfd
Fleet-git <51721134+Fleet-git@users.noreply.github.com>
2020-03-05 06:43:26 -0500

    Update TestBase.java

--- src/main/java/Resources/TestBase.java
+++ src/main/java/Resources/TestBase.java
@@ -31,7 +31,14 @@ public WebDriver initializeDriver() throws IOException
 //if(browserName.equals("chrome"))
 //{
 	System.setProperty("webdriver.chrome.driver","chromedriver");
-	return driver= new ChromeDriver();
+	//return driver= new ChromeDriver();
+	ChromeOptions opt = new ChromeOptions();
+	options.binary_location = "chrome.exe"
+	options.addArguments("start-maximized");
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        options.setExperimentalOption("useAutomationExtension", false);
+        WebDriver driver = new ChromeDriver(opt);
+        driver.get("https://www.google.com/");
 	
 }
 
